01f80fc501b436717d42e2b9e4f4135ab9164939
Краснова Татьяна <redtanya@yandex.ru>
2022-06-14 16:22:46 +0300

    mistake post.birthday = new FormDate()

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+}
